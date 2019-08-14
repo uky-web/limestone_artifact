f4eaf649c7f350740dd6b1030123cba73426ee7a
Andrew Shooner <andrew.shooner@uky.edu>
2019-08-14 22:27:46 +0000

    CI BUILD

--- js/link-group-dropdown.js
+++ js/link-group-dropdown.js
@@ -23,7 +23,6 @@ var link_group_dropdown = function link_group_dropdown() {
     });
 
     $(window).on('click', function (e) {
-
         // if the target was not within the expanded dropdown
         console.log($(e.target).parents('.link-group-dropdown'));
         if ($(e.target).parents('.link-group-dropdown').length < 1) {
