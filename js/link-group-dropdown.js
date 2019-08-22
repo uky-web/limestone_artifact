59af2936133fb11fbd17599fd1e93f918d4eecf5
Nicole <nrsa222@uky.edu>
2019-08-22 21:01:24 +0000

    CI BUILD  add link-group-dropdown file back into repo

---
+++ js/link-group-dropdown.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var link_group_dropdown = function link_group_dropdown() {
+
+    $('.link-group-dropdown > a').each(function () {
+        $(this).on('click', function () {
+            var $slabEl = $(this).parents('.link-group-dropdown');
+
+            if ($slabEl.attr('aria-expanded') === 'false') {
+                $slabEl.attr('aria-expanded', 'true');
+            } else {
+                $slabEl.attr('aria-expanded', 'false');
+            }
+        });
+
+        $(this).on('keydown', function () {
+            var $slabEl = $(this).parents('.link-group-dropdown');
+
+            if ($slabEl.attr('aria-expanded') === 'false') {
+                $(this).click();
+            }
+        });
+    });
+
+    $(window).on('click', function (e) {
+        // if the target was not within the expanded dropdown
+        console.log($(e.target).parents('.link-group-dropdown'));
+        if ($(e.target).parents('.link-group-dropdown').length < 1) {
+            $('.link-group-dropdown[aria-expanded="true"]').attr('aria-expanded', 'false');
+        }
+    });
+};
+//# sourceMappingURL=link-group-dropdown.js.map
